a7c83100ac6c36cc0ec455e5e410c8c676af7036
santtonas <juaosanttosx@gmail.com>
2026-05-07 23:22:55 -0300

    Atualizado o arquivo de rotinas

--- proefigi/src/routes.jsx
+++ proefigi/src/routes.jsx
@@ -1,34 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import  {Home}  from "./pages/home";
-
+import { Home } from "./pages/home";
 import Layout from './components/Layout';
 import Login from './pages/Login';
 import Cadastro from "./pages/Cadastro";
 import Pomodoro from "./pages/Pomodoro";
 import Progresso from "./pages/Progresso";
 import Metas from "./pages/Metas";
-import {Calendario} from "./pages/Calendario";
-import {TarefaProvider} from './context/TarefaContext';
-
+import Rotina from "./pages/Rotina"; 
+import { Calendario } from "./pages/Calendario";
+import { TarefaProvider } from './context/TarefaContext';
 
 function RoutesApp() {
   return (
     <BrowserRouter>
-    <TarefaProvider>
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/cadastro" element={<Cadastro/>}/>
+      <TarefaProvider>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/cadastro" element={<Cadastro/>}/>
 
-        <Route element={<Layout />}>
-          <Route path="/calendario" element={<Calendario />} />
-          <Route path="/pomodoro" element={<Pomodoro/> }/>
-          <Route path="/progresso" element={<Progresso/>}/>
-          <Route path="/metas" element={<Metas/>}/>
-          <Route path="/home" element={<Home/>}/>
-        </Route>
-
-      </Routes>
-    </TarefaProvider>
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/calendario" element={<Calendario />} />
+            <Route path="/pomodoro" element={<Pomodoro/> }/>
+            <Route path="/progresso" element={<Progresso/>}/>
+            <Route path="/metas" element={<Metas/>}/>
+            <Route path="/rotina" element={<Rotina/>}/> 
+          </Route>
+        </Routes>
+      </TarefaProvider>
     </BrowserRouter>
   );
 }
